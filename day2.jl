1bcb83d59e2225b778847bd8e66c37d9c4f3f1c3
recke96 <jakob.ecker42@gmail.com>
2025-12-02 17:29:22 +0000

    Solution day 2 pt 2

--- day2.jl
+++ day2.jl
@@ -6,23 +6,45 @@ function (@main)(args)
         v -> Iterators.map(x -> parse.(Int, x), v) |>
         v -> Iterators.flatmap(x -> x[1]:x[2], v) |>
         v -> Iterators.filter(is_invalid_pt1, v) |>
-        sum
+       #v -> Iterators.filter(is_invalid_pt2, v) |>
+        v -> Base.sum(v; init=0)
 
-    @info "Sum of invalid pt1 ids: $(t)"
+    @info "Sum of invalid ids: $(t)"
 end
 
 function is_invalid_pt1(num::Int)::Bool
     str_num = string(num)
-    if length(str_num) % 2 != 0
-        return false
-    end
+    length(str_num) % 2 != 0 && return false
     stop = length(str_num) ÷ 2
-    for i in 1:stop
-        if str_num[i] != str_num[stop + i]
+    pattern = @view str_num[1:stop]
+    return matches_pattern(pattern, str_num)
+end
+
+function is_invalid_pt2(num::Int)::Bool
+    num_str = string(num)
+    len = length(num_str)
+    pattern_lenghts = range(len ÷ 2, 1; step=-1)
+    @debug "pattern length $(pattern_lenghts) for number $(num)"
+    for pattern_len in pattern_lenghts
+        len % pattern_len != 0 && continue
+        pattern = @view num_str[1:pattern_len]
+        matches_pattern(pattern, num_str) && return true
+    end
+
+    return false
+end
+
+function matches_pattern(pattern::AbstractString, string::AbstractString)::Bool
+    for start in range(length(pattern) + 1, length(string); step=length(pattern))
+        stop = start + length(pattern) - 1
+        stop > length(string) && return false
+        segment = @view string[start:stop]
+        @debug "Comparing pattern $(pattern) to segment $(segment) at position $(start)"
+        if segment != pattern
             return false
         end
     end
-    @debug "Found invalid pt1 id: $(num)"
+    @debug "Pattern $(pattern) matches string $(string)"
     return true
 end
 
