b5f299690c2ac68eda3db01c402e1ff1751ff1d9
recke96 <jakob.ecker42@gmail.com>
2025-12-03 18:43:44 +0000

    generalize solution so it also works with pt 2

--- day3.jl
+++ day3.jl
@@ -1,23 +1,30 @@
 import Logging
+import Base.Iterators: drop, map
 
 function (@main)(args)
     total_joltage = eachline(args[1]) |>
-    l -> Iterators.map(find_max_joltage, l) |>
+    l -> map(b -> max_joltage(b, 12), l) |>
     sum
 
     @info "Total joltage: $(total_joltage)"
 end
 
-function find_max_joltage(bank::AbstractString)::Int
-   active = (bank[1], bank[2])
-   for i in Iterators.drop(eachindex(bank), 2)
-       if bank[i-1] > active[1]
-           active = (bank[i-1], bank[i])
-       end
-       if bank[i] > active[2]
-           active = (active[1], bank[i])
-       end
-   end
+function max_joltage(bank::AbstractString, active_bats::Int)::Int
+    active = collect(bank[1:active_bats])
+    for i in drop(eachindex(bank), active_bats)
+        for j in eachindex(active)
+            bankidx = i - active_bats + j
+            if bank[bankidx] > active[j]
+                @debug "updating active batteries at positions $(j:active_bats) with bank positions $(bankidx:i)"
+                active[j:active_bats] .= collect(bank[bankidx:i])
+                break
+            end
+        end
+    end
 
-   return parse(Int, active[1] * active[2])
+    active_str = join(active)
+
+    @debug "Max joltage for bank $(bank) with $(active_bats) batteries is $(active_str)"
+
+    return parse(Int, active_str)
 end
