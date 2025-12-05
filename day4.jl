3330bdca70365a6c46bee8e5c07c479bc6c7fdfe
recke96 <jakob.ecker42@gmail.com>
2025-12-05 10:50:35 +0000

    small rework

--- day4.jl
+++ day4.jl
@@ -6,19 +6,30 @@ function (@main)(args)
                 l -> map(c -> reduce(hcat, c), l) |>
                      l -> reduce(vcat, l)
 
+    accessible_rolls = @timed pt1(grid)
+
+    @info "Accessible rolls: $(accessible_rolls.value)"
+    @info "Stats: $(accessible_rolls)"
+end
+
+function is_accessible(grid::AbstractMatrix{Char}, col::Int, row::Int)::Bool
+    adjecent_rolls = (get(grid, (row + dr, col + dc), '.') for dr in -1:1, dc in -1:1 if (dr != 0 || dc != 0)) |>
+                     n -> count(c -> c == '@', n)
+
+    return adjecent_rolls < 4
+end
+
+function pt1(grid::AbstractMatrix{Char})
     accessible_rolls = 0
     for col = axes(grid, 2), row = axes(grid, 1)
         if grid[row, col] == '.'
             continue
         end
 
-        adjecent_rolls = (get(grid, (row + dr, col + dc), '.') for dr in -1:1, dc in -1:1 if (dr != 0 || dc != 0)) |>
-                         n -> count(c -> c == '@', n)
-
-        if adjecent_rolls < 4
+        if is_accessible(grid, col, row)
             accessible_rolls += 1
         end
     end
 
-    @info "Accessible rolls: $(accessible_rolls)"
+    return accessible_rolls
 end
