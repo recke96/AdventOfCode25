aa5ed7125d99f7e068054bbbe6c839b28dcacbe8
recke96 <jakob.ecker42@gmail.com>
2025-12-06 13:31:34 +0000

    solve day 6 pt 2

--- day6.jl
+++ day6.jl
@@ -1,6 +1,6 @@
 
 function (@main)(args)
-    grand_total = @timed pt1(args[1])
+    grand_total = @timed pt2(args[1])
 
     @info "Grand Total: $(grand_total.value)"
     @info "Stats: $(grand_total)"
@@ -25,7 +25,46 @@ function pt1(file::AbstractString)::Int
             continue
         end
 
-        subtotals = broadcast((op, a, b) -> op(a, b), operators, values, subtotals)     
+        subtotals = broadcast((op, a, b) -> op(a, b), operators, values, subtotals)
     end
     return sum(subtotals)
+end
+
+function pt2(file::AbstractString)::Int
+    lines = readlines(file)
+    operators_line = pop!(lines)
+
+    grid = lines |>
+           l -> map(c -> reduce(hcat, c), l) |>
+                l -> reduce(vcat, l)
+
+    columns = find_columns(grid)
+    operators = map(o -> o == "+" ? (+) : (*), split(operators_line))
+
+    total = 0
+    for op in operators
+        column = popfirst!(columns)
+        subtotal = reduce(op, column)
+        total += subtotal
+    end
+
+    return total
+end
+
+function find_columns(lines::Matrix{Char})::Vector{Vector{Int}}
+    columns = Vector{Vector{Int}}()
+    current = Vector{Int}()
+    for col in axes(lines, 2)
+        if all(lines[:, col] .== ' ')
+            push!(columns, current)
+            current = Vector{Int}()
+            continue
+        end
+
+        value = parse(Int, String(lines[:, col]))
+        push!(current, value)
+    end
+
+    push!(columns, current)
+    return columns
 end
